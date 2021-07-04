7366878001c9b741d90a6bb2dfc9551b2c665773
Migada Perera <mp@pop-os.localdomain>
2021-07-04 19:56:45 +1200

    trying to get the radial buttons to work the way I want them to.

--- app.py
+++ app.py
@@ -10,182 +10,182 @@ app.secret_key = "adfaef3234fwef4rg423refaewsf"
 
 
 def create_connection(db_file):
-	try:
-		connection = sqlite3.connect(db_file)
-		return connection
-	except Error as e:
-		print(e)
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
 
 
 def return_page(page, **extras):
-	con = create_connection("maori_dictionary.db")
+    con = create_connection("maori_dictionary.db")
 
-	query = "SELECT categoryid, category  FROM category"
+    query = "SELECT categoryid, category  FROM category"
 
-	cur = con.cursor()
-	cur.execute(query)
-	categorylist = dict(cur.fetchall())
-	con.close
+    cur = con.cursor()
+    cur.execute(query)
+    categorylist = dict(cur.fetchall())
+    con.close
 
-	return render_template(page, categories=categorylist, **extras)
+    return render_template(page, categories=categorylist, **extras)
 
 
 @app.route('/')
 def render_home():
-	return return_page('home.html')
+    return return_page('home.html')
 
 
 @app.route('/words', methods=['GET', 'POST'])
 def render_menu_page():
-	con = create_connection("maori_dictionary.db")
+    con = create_connection("maori_dictionary.db")
 
-	query = "SELECT maori, english, category, definintion, level, picture FROM dictionary_values"
+    query = "SELECT maori, english, category, definintion, level, picture FROM dictionary_values"
 
-	cur = con.cursor()
-	cur.execute(query)
-	dictionary_values = cur.fetchall()
-	con.close()
+    cur = con.cursor()
+    cur.execute(query)
+    dictionary_values = cur.fetchall()
+    con.close()
 
-	return return_page('words.html', dictionary_values=dictionary_values)
+    return return_page('words.html', dictionary_values=dictionary_values)
 
 
 @app.route('/categorypage')
 def render_categorypage():
-	categoryid = request.args.get("category")
-	print(categoryid)
+    categoryid = request.args.get("category")
+    print(categoryid)
 
-	con = create_connection("maori_dictionary.db")
+    con = create_connection("maori_dictionary.db")
 
-	query = "SELECT id, maori, english, category, definition, level, picture FROM dictionary_values WHERE category=?"
+    query = "SELECT id, maori, english, category, definition, level, picture FROM dictionary_values WHERE category=?"
 
-	cur = con.cursor()
-	cur.execute(query, (categoryid, ))
-	words = cur.fetchall()
-	print(words)
-	con.close()
+    cur = con.cursor()
+    cur.execute(query, (categoryid, ))
+    words = cur.fetchall()
+    print(words)
+    con.close()
 
-	con = create_connection("maori_dictionary.db")
+    con = create_connection("maori_dictionary.db")
 
-	query = "SELECT categoryid, category  FROM category WHERE categoryid=?"
+    query = "SELECT categoryid, category  FROM category WHERE categoryid=?"
 
-	cur = con.cursor()
-	cur.execute(query, (categoryid, ))
-	categoryname = cur.fetchall()[0][1]
-	return return_page('categorypage.html', words=words, categoryname=categoryname)
+    cur = con.cursor()
+    cur.execute(query, (categoryid, ))
+    categoryname = cur.fetchall()[0][1]
+    return return_page('categorypage.html', words=words, categoryname=categoryname)
 
 
 @app.route('/wordpage')
 def render_wordpage():
-	id = request.args.get("word")
-	print(id)
+    id = request.args.get("word")
+    print(id)
 
-	con = create_connection("maori_dictionary.db")
+    con = create_connection("maori_dictionary.db")
 
-	query = "SELECT maori, english, category, definition, level, picture FROM dictionary_values WHERE id=?"
+    query = "SELECT maori, english, category, definition, level, picture FROM dictionary_values WHERE id=?"
 
-	cur = con.cursor()
-	cur.execute(query, (id, ))
+    cur = con.cursor()
+    cur.execute(query, (id, ))
 
-	one = cur.fetchall()[0]
-	print(one)
-	con.close()
+    one = cur.fetchall()[0]
+    print(one)
+    con.close()
 
-	return return_page('wordpage.html', oneword=one)
+    return return_page('wordpage.html', oneword=one)
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def render_signup():
-	if request.method == 'POST':
-		print(request.form)
-		fname = request.form.get('fname').strip().title()
-		lname = request.form.get('lname').strip().title()
-		email = request.form.get('email').strip().lower()
-		password = request.form.get('password')
-		password2 = request.form.get('password2')
+    if request.method == 'POST':
+        print(request.form)
+        fname = request.form.get('fname').strip().title()
+        lname = request.form.get('lname').strip().title()
+        email = request.form.get('email').strip().lower()
+        password = request.form.get('password')
+        password2 = request.form.get('password2')
 
-		if password != password2:
-			return redirect("/signup?error=Passwords+dont+match")
+        if password != password2:
+            return redirect("/signup?error=Passwords+dont+match")
 
-		if len(password) < 8:
-			return redirect('/signup?error=Password+must+be+8+characters+or+more')
+        if len(password) < 8:
+            return redirect('/signup?error=Password+must+be+8+characters+or+more')
 
-		hashed_password = bcrypt.generate_password_hash(password)
+        hashed_password = bcrypt.generate_password_hash(password)
 
 
-		con = create_connection('maori_dictionary.db')
+        con = create_connection('maori_dictionary.db')
 
-		query = "INSERT INTO users (id, fname, lname, email, password) " \
-				"VALUES(NULL,?,?,?,?)"
+        query = "INSERT INTO users (id, fname, lname, email, password) " \
+                "VALUES(NULL,?,?,?,?)"
 
-		cur = con.cursor()  # You need this line next
+        cur = con.cursor()  # You need this line next
 
-		try:
-			cur.execute(query, (fname, lname, email, hashed_password))  # this line actually executes the query
-		except sqlite3.IntegrityError:
-			return redirect('/signup?error=Email+is+already+used')
+        try:
+            cur.execute(query, (fname, lname, email, hashed_password))  # this line actually executes the query
+        except sqlite3.IntegrityError:
+            return redirect('/signup?error=Email+is+already+used')
 
-		con.commit()
-		con.close()
+        con.commit()
+        con.close()
 
-	return return_page('signup.html')
+    return return_page('signup.html')
 
 
 @app.route('/addword', methods=['GET', 'POST'])
 def render_addword():
-	if request.method == 'POST':
-		print(request.form)
-		maori = request.form.get('maori').strip()
-		english = request.form.get('english').strip()
-		category = request.form.get('category').strip()
-		level = request.form.get('level')
-		definition = request.form.get('definition').title()
+    if request.method == 'POST':
+        print(request.form)
+        maori = request.form.get('maori').strip()
+        english = request.form.get('english').strip()
+        category = request.form.get('category').strip()
+        level = request.form.get('level')
+        definition = request.form.get('definition').title()
 
-		con = create_connection('maori_dictionary.db')
+        con = create_connection('maori_dictionary.db')
 
-		query = "INSERT INTO dictionary_values (maori, english, category, definition, level) " \
-				"VALUES(?,?,?,?,?)"
+        query = "INSERT INTO dictionary_values (maori, english, category, definition, level) " \
+                "VALUES(?,?,?,?,?)"
 
-		cur = con.cursor()  # You need this line next
+        cur = con.cursor()  # You need this line next
 
-		try:
-			cur.execute(query, (maori, english, category, definition, level))  # this line actually executes the query
-		except sqlite3.IntegrityError:
-			return redirect('/addwords?error=you+screwed+something+up')
+        try:
+            cur.execute(query, (maori, english, category, definition, level))  # this line actually executes the query
+        except sqlite3.IntegrityError:
+            return redirect('/addwords?error=you+screwed+something+up')
 
-		con.commit()
-		con.close()
-	return return_page('addword.html')
+        con.commit()
+        con.close()
+    return return_page('addword.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def render_login():
-	  if request.method == "POST":
-	  	email = request.form['email'].strip().lower()
-		password = request.form['password'].strip()
+      if request.method == "POST":
+        email = request.form['email'].strip().lower()
+        password = request.form['password'].strip()
 
-		query = "SELECT id, fname, password FROM users WHERE email = ?"
-		con = create_connection('maori_dictionary.db')
-		cur = con.cursor()
-		cur.execute(query, (email,))
-		user_data = cur.fetchall()
-		con.close()
-		# if given the email is not in the database this will raise an error
-		# would be better to find out how to see if the query return an empty resultset
-		try:
-			userid = user_data[0][0]
-			fname = user_data[0][1]
-			db_password = user_data[0][2]
-		except IndexError:
-			return redirect("/login?error=Email+fnameinvalid+or+password+incorrect")
+        query = "SELECT id, fname, password FROM users WHERE email = ?"
+        con = create_connection('maori_dictionary.db')
+        cur = con.cursor()
+        cur.execute(query, (email,))
+        user_data = cur.fetchall()
+        con.close()
+        # if given the email is not in the database this will raise an error
+        # would be better to find out how to see if the query return an empty resultset
+        try:
+            userid = user_data[0][0]
+            fname = user_data[0][1]
+            db_password = user_data[0][2]
+        except IndexError:
+            return redirect("/login?error=Email+fnameinvalid+or+password+incorrect")
 
-		# check if the password is incorrect for that email address
+        # check if the password is incorrect for that email address
 
-		if not bcrypt.check_password_hash(db_password, password):
-			return redirect(request.referrer + "?error=Email+invalid+or+password+incorrect")
+        if not bcrypt.check_password_hash(db_password, password):
+            return redirect(request.referrer + "?error=Email+invalid+or+password+incorrect")
 
-	  return redirect('/')
+      return redirect('/')
 
-	  return return_page('login.html')
+      return return_page('login.html')
 
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
